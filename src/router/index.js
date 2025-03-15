6581342a9c6ab59123dd3cdda1671ac05f5b7257
0 ，1   ，0 <oncwnuJdn838qBriWdOLA7qLNohk@git.weixin.qq.com>
2025-03-16 01:26:25 +0800

    change router of RoomVIew to right

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import RoomsView from '../views/RoomsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import RoomsView from "../views/RoomsVIew.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/', redirect: '/Home'},
-    {path: '/Home',name: 'Home',component: HomeView},
-    {path: '/About',name: 'About',component: AboutView},
-    {path: '/Rooms',name: 'Rooms',component: RoomsView}
+    { path: "/", redirect: "/Home" },
+    { path: "/Home", name: "Home", component: HomeView },
+    { path: "/About", name: "About", component: AboutView },
+    { path: "/Rooms", name: "Rooms", component: RoomsView },
   ],
-})
+});
 
-export default router
+export default router;
